68d3f769a228195b5262423fe58febcc1a6384f8
Vikas Umbadwar <vikasumbadwar@gmail.com>
2024-02-28 12:42:56 +0530

    updated

---
+++ 02_Datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all js code as newer version
+
+//alert( 3 + 3) 
+// we are using nodejs , not browser
+
+console.log(3
+    + 3) // code readebility it shoulde be goog so avoid that ...
+
+    console.log("vikas");
+let name = "hitesh"
+let age = 19
+let isLoggedIn = false
+
+// number => 2 to pwer 53
+//bigint 
+//string => ""
+//boolean => true or false
+//null standalone value
+//undefined => abhi value assign nahi hui hai
+//symbol  => unique 
+//object => main
+
+console.log(typeof("vikas"))
+console.log(typeof(undefined))
+console.log(typeof (null));
