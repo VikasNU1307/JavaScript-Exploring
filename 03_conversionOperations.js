361c2bb4039818827539991b2239125e0718d473
Vikas Umbadwar <vikasumbadwar@gmail.com>
2024-02-28 13:08:54 +0530

    operation added

--- 03_conversionOperations.js
+++ 03_conversionOperations.js
@@ -23,5 +23,30 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33;
 let StringNumber = String(someNumber)
-console.log(StringNumber);
-console.log(typeof StringNumber)
+// console.log(StringNumber);
+// console.log(typeof StringNumber)
+
+// *************************************** Operations *********************************** /
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+console.log(2**2) // basically 2 the power 2 
+
+let str1 = "hello"
+let str2 = " vikas"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
+
+
+
+
+
