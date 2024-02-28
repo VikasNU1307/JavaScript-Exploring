ad21bd880822a005197a56ee47a8b7f6c2065352
Vikas Umbadwar <vikasumbadwar@gmail.com>
2024-02-28 12:53:56 +0530

    convesionOperation

--- 03_conversionOperations.js
+++ 03_conversionOperations.js
@@ -1,3 +1,27 @@
-let  score = 33
+let  score = "33aa"
 
-console.log(typeof score);
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// conversion of string to number 
+let valueInNumber = Number(score)
+// console.log( typeof valueInNumber)
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true = > 1; false => 0
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1  => true; 0 => fasle
+// "" => false
+// "vikas" => true;
+
+let someNumber = 33;
+let StringNumber = String(someNumber)
+console.log(StringNumber);
+console.log(typeof StringNumber)
